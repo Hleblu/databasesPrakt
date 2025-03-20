c01d18471332488e7b5ea0a6be26df76977c95b3
Hleblu <156907741+Hleblu@users.noreply.github.com>
2025-03-20 08:03:34 +0100

    prakt3

--- requests.sql
+++ requests.sql
@@ -3,7 +3,15 @@ SELECT workers.first_name, workers.last_name, workers.middle_name
 FROM workers;
 
 --2
-SELECT goods.name, goods.price*9.5 AS total_price
+SELECT goods.name, round(goods.price*9.5, 2) AS total_price
 FROM goods;
 
 --3
+SELECT goods.*
+FROM goods
+ORDER BY goods.name;
+
+--4
+SELECT workers.*
+FROM workers
+ORDER BY workers.qualification, passport_no DESC;
